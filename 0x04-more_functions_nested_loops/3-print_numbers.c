3c099faf7025dde10836b7e369be01f38053a90d
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-17 13:30:04 -0700

    Prints all numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,23 +4,18 @@
 /**
  * print_numbers - prints the numbers from 0 to 9, followed by a new line
  *
- * Return: 0
+ * Return: no return
  */
 void print_numbers(void)
 {
 	/* loop counter declaration */
 	int i;
 
-	/* assign initial value from where we want to print the numbers */
-	i = 0;
-
-	/* while loop, that will print numbers from 0 to 9 */
-	while (i <= 9)
+	/* loop goes while i < 9, and i increases by one every loop */
+	for (i = 48; i < 58; i++)
 	{
-		/*printing the numbers */
-		_putchar("%d\n", i);
-		/* increment */
-		i++;
+		_putchar(i);
 	}
+
 	_putchar("\n");
 }
