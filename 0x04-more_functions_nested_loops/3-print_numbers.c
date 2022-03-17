4a3ccd8ba58b8086bfe6843b1c7360e7cf51002c
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-17 13:35:02 -0700

    Prints all numbers from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,14 +8,11 @@
  */
 void print_numbers(void)
 {
-	/* loop counter declaration */
 	int i;
 
-	/* loop goes while i < 9, and i increases by one every loop */
 	for (i = 48; i < 58; i++)
 	{
 		_putchar(i);
 	}
-
-	_putchar("\n");
+	_putchar('\n');
 }
