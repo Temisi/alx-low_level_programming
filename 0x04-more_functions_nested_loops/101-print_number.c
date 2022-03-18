2f2b6ff1547987bbd9cee2d8811f990f572940d2
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-17 21:06:27 -0700

    Prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,21 +9,29 @@
 
 void print_number(int n)
 {
-	unsigned int m;
+	unsigned int m, d, count;
 
 	if (n < 0)
 	{
-		m = -n;
 		_putchar(45);
+		m = n * -1;
 	}
 	else
 	{
 		m = n;
 	}
 
-	if (m / 10)
+	d = m;
+	count = 1;
+
+	while (d > 9)
 	{
-		print_number(m / 10);
+		d /= 10;
+		count *= 10;
 	}
-	_putchar((m % 10) + 48);
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
