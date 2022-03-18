426f965a23b84875aa605b3acf8e3a0ed687f566
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-17 17:41:35 -0700

    Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,20 +11,18 @@ int main(void)
 {
 	int n;
 
-	n = 1;
-	printf("%d", n);
 
-	for (n = 2; n <= 100; n++)
+	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
 		}
-		else if (n % 3 == 0)
+		else if ((n % 3 == 0) && (n % 5 != 0))
 		{
 			printf(" Fizz");
 		}
-		else if (n % 5 == 0)
+		else if ((n % 5 == 0) && (n % 3 != 0))
 		{
 			printf(" Buzz");
 		}
