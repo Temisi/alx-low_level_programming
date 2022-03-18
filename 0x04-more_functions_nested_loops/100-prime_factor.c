8c1174080759ac610987a1c47b14919234e1df78
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-17 18:14:52 -0700

    Header files

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - prints the largest prime factor of a given number
+ * followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int getMaxPrimeFactor(int n)
+{
+	int i, max = -1;
+	n = 612852475143;
+
+	while (n % 2 == 0)
+	{
+		max = 2;
+		n /= 2; /* reduce n by dividing this by 2 */
+	}
+
+	for (i = 3; i <= sqrt(n); i = i + 2)
+	{ 
+		/* i will increase by 2, to get only odd numbers */
+		while (n % i == 0)
+		{
+			max = i;
+			n /= i;
+		}
+	}
+
+	if (n > 2)
+	{
+		max = n;
+	}
+	printf("%1d\n", max);
+
+	return (0);
+}
