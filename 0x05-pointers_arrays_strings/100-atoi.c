b7f04acfe06c8814622f3de0c332d38eac5224b5
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-22 10:14:37 -0700

    Function to convert string to integer

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,35 +11,22 @@
 
 int _atoi(char *s)
 {
-	int i = 0;
+	int c = 1, b = 0, i = 0;
 
-	while (i < s.length && s[i] == ' ')
+	while (s[i] == ' ')
 	{
 		i++;
-
-		if (i >= s.length)
-		{
-			return (0);
-		}
 	}
-
-	int c = 1;
 
 	if (s[i] == 45 || s[i] == 43)
 	{
 		c = 1 - 2 * (s[i++] == 45);
 	}
 
-	if (i >= s.length)
+	while (s[i] >= 48 && s[i] <= 57)
 	{
-		return (0);
-	}
-
-	int b = 0;
-
-	for (int i = 0; i < s.length && s[i] >= 48 && s[i] <= 57; i++)
-	{
-		if (b > INT_MAX / 10 || (b == INT_MAX / 10 && s[i] - 48 > INT_MAX % 10))
+		if (b > INT_MAX / 10 ||
+		(b == INT_MAX / 10 && s[i] - 48 > 7))
 		{
 			if (c == 1)
 				return (INT_MAX);
