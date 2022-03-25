f805b91a95e3d9c21c5298e3cb5c5a13e6b5f064
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-24 18:27:30 -0700

    Source code to convert lowercase letters in a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase
+ * character/letter of a string to
+ * uppercase.
+ * @s: string input
+ *
+ * Return: the pointer to dest.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+		{
+			s[i] = s[i] - 32;
+		}
+		i++;
+	}
+
+	return (s);
+}
