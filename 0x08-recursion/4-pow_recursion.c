092d2458d7c393abe243e08e0d8d2bda2bfdabcf
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-03-31 16:44:41 -0700

    Function to carry out exponential of two values

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _pow_recursion - returns x raised to the power of y
+ * @x: first interger/base
+ * @y: second integer/exponent
+ *
+ * Return: value of the exponential
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		return (x * _poe_recursion(x, y - 1));
+}
