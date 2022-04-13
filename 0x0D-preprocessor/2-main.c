45a53ef424dbaf60873090cc1e198d12359bdc32
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-04-12 22:17:09 -0700

    Program to print the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Description: A program that prints the name of the file it was
+ * compiled from
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
