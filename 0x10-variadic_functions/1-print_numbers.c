7f64dd6499ecf3907afb9d6e49714cd6d33e7b74
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-04-16 06:35:33 -0700

    Function that prints numbers, followed by new line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -3,7 +3,7 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - print numn=bers followed by new line
+ * print_numbers - print numbers followed by new line
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  *
