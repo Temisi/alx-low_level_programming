1f60944343817ff088daf59eebadfafc0da013a4
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-04-15 19:56:05 -0700

    Program to perform simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,41 +4,41 @@
 
 /**
  * main - Entry point
- * Description: A program that performs simple operations
- * @argc: Number of cli arguments
- * @argv: Vector cli arguments
+ * Description: Program that performs simple operations
+ * @argc: count argument
+ * @arcv: vector argument
  *
  * Return: 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int a, b;
 	int (*operation)(int, int);
+	int a, b;
 
 	if (argc != 4)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(98);
 	}
 
-	if (argv[2][1])
+	if (get_op_func(argv[2]) == NULL)
 	{
-		printf("Error\n");
+		puts("Error");
 		exit(99);
+	}
+
+	if ((*argv[2] == '/' || *argv[2] == '%') && atoi(argv[3]) == 0)
+	{
+		puts("Error");
+		exit(100);
 	}
 
 	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
 	printf("%d\n", operation(a, b));
+
 	return (0);
 }
