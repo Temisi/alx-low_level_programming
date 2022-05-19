dbce7f31a7a2291832da12a7a7841680d587a905
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-05-19 11:27:20 -0700

    task 1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,14 @@
 
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t num = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
+		num++;
 		h = h->next;
-		nnodes++;
 	}
-	return (nnodes);
+
+	return (num);
 }
