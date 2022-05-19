5979ee2c87811dbf784e5afe3bf017f448b2ccfd
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-05-19 11:37:41 -0700

    Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * print_listint - prints aall the elements of a list
+ * print_listint - prints all the elements of a list
  * @h: head of a list
  *
  * Return: number of nodes
