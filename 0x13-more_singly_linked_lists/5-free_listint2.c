b3313d7cb5abc2dfdfc82bcfce4e51bacc54fa44
Fehintola Akinsete <akinsetefehintola997@gmail.com>
2022-05-20 11:03:46 -0700

    Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list
+ *
+ * Return: no return
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *purr;
+
+	if (head != NULL)
+	{
+		purr = *head;
+
+		while ((temp = purr) != NULL)
+		{
+			purr = purr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
